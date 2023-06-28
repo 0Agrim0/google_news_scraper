9042799a4ecc7593e6ad2ec2f7b666476ae2af7c
0Agrim0 <agrim.gupta@crofarm.in>
2023-06-28 18:09:31 +0530

    search_by_name

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from scraper_manager import  scraper_manager
 def main():
     s_m = scraper_manager()
     try:
-        topics=s_m.topics()
-        print('Choose the topic number:')
-        topic_id=input()
+        n=s_m.two_options()
+        if n=="2":
+            topic = s_m.topics()
+            urls = s_m.get_url(topic)
+            data = s_m.page_data(urls)
+            s_m.scrap_data_from_page(data)
+            s_m.dump_data()
 
-        urls=s_m.get_url(topics[topic_id])
-        data=s_m.page_data(urls)
-        s_m.scrap_data_from_page(data)
-        s_m.dump_data()
+        elif n=="1":
+            urls=s_m.search_by_name()
+            data=s_m.page_data(urls)
+            s_m.scrap_data_from_name_page(data)
+            s_m.dump_data()
+
+
 
     except:
         s_m.dump_data()
--- scraper_manager.py
+++ scraper_manager.py
@@ -10,24 +10,38 @@ class scraper_manager:
 
     def __init__(self):
         file_name="_".join(str(datetime.datetime.now().date()).split("-"))+".csv"
-        print("The data is going to store in this file: {} ".format(file_name))
+        print("The data is going to store in this file: {}   ".format(file_name))
         self.topic="home"
         self.url="https://news.google.com"
         self.data=[]
         self.file_name=file_name
 
+
+    def two_options(self):
+        options={
+            "1":"Search by name",
+            "2":"Search by topic"
+        }
+        print(options)
+        n=input()
+        return n
+
+
     def topics(self):
-        topic_dict={"1":"India",
-                    "2":"World",
-                    "3":"Business",
-                    "4":"Technology",
-                    "5":"Entertainment",
-                    "6":"Sports",
-                    "7":"Science",
-                    "8":"Health",
-                    "9":"ALL"}
+        topic_dict = {"1": "India",
+                      "2": "World",
+                      "3": "Business",
+                      "4": "Technology",
+                      "5": "Entertainment",
+                      "6": "Sports",
+                      "7": "Science",
+                      "8": "Health",
+                      "9": "ALL"}
         print(topic_dict)
-        return topic_dict
+        print('Choose the topic number:')
+        topic_id = input()
+
+        return topic_dict[topic_id]
 
     def get_url(self,topic_name):
         final_url=[]
@@ -62,7 +76,7 @@ class scraper_manager:
 
 
 
-    def scrap_data_from_page(self,data,):
+    def scrap_data_from_page(self,data):
 
         for text_data in data:
             d=datetime.datetime.now()
@@ -72,8 +86,23 @@ class scraper_manager:
                 self.data.append([d,tag.text,text_data[1]])
 
 
+    def scrap_data_from_name_page(self,data):
+        for text_data in data:
+            d = datetime.datetime.now()
+            soup = BeautifulSoup(text_data[0], 'html.parser')
+            h4_tags = soup.find_all('h3')
+            for tag in h4_tags:
+                self.data.append([d, tag.text, text_data[1]])
+
     def dump_data(self):
         df=pd.DataFrame(self.data,columns=['Date','News','Type'])
         df.to_csv(self.file_name,index=False)
         print("----------------------------------------------Data-Store-------------------------------------------------------------")
 
+
+
+
+    def search_by_name(self):
+        name=input("Write the name: ")
+        api = [["https://news.google.com/search?q={}&hl=en-IN&gl=IN&ceid=IN%3Aen".format(name),name]]
+        return api
